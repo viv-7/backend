95ecf5b5a032f138247a95bf822bf7544f72e23c
viv-7 <180554941+viv-7@users.noreply.github.com>
2025-12-10 23:00:54 +0530

    feat: setup database connection and update project dependencies
    - Added dotenv and mongoose as dependencies in package.json
    - Created a new constants.js file to store database name
    - Implemented database connection logic in db.js
    - Updated index.js to establish a connection to the database
    - Modified dev script in package.json to support dotenv and experimental JSON modules

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// require("dotenv").config();  // CommonJS syntax
+import connectDB from "./db/db.js";
+import { connect } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./env"
+});
+
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       
+        app.on("error", (error) => {
+            console.error("ERROR", error);
+            throw error
+        })
+       
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+        
+    }catch (error) {
+        console.error("ERROR", error)
+        throw error
+    }
+})()
+*/
